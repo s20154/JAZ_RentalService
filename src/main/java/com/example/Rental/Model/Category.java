fb897c5a543f5ec27d17ae039e72586c19eec014
Kijanczuk, Damian <kijaczukdamian@gmail.com>
2021-06-05 16:54:07 +0200

    Second commit

---
+++ src/main/java/com/example/Rental/Model/Category.java
@@ -0,0 +1,14 @@
+package com.example.Rental.Model;
+
+public enum Category {
+    ACTION,
+    COMEDY,
+    DRAMA,
+    FANTASY,
+    HORROR,
+    MYSTERY,
+    ROMANCE,
+    THRILLER,
+    WESTERN,
+    SCIFI
+}
